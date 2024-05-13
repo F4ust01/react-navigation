ecfe27e7af78ac4b99791d9757010d0922b23557
Fausto Cuquejo <ofaustojoaquindejesus@gmail.com>
2024-05-13 12:50:47 -0300

    se agregó flatlist y size matters al proyecto

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,46 +1,59 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Button, Image } from "react-native";
+import { View, Text, Button, Image, FlatList } from "react-native";
 import axios from "axios";
+import { moderateScale } from "react-native-size-matters";
 
 const HomeScreen = ({ navigation }) => {
-  const [randomPokemon, setRandomPokemon] = useState(null);
+  const [randomPokemons, setRandomPokemons] = useState([]);
 
   useEffect(() => {
-    fetchRandomPokemon();
+    fetchRandomPokemons();
   }, []);
 
-  const fetchRandomPokemon = async () => {
+  const fetchRandomPokemons = async () => {
     try {
-      const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
-      const randomIndex = Math.floor(
-        Math.random() * response.data.results.length
+      const response = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=10"
       );
-      const randomPokemonUrl = response.data.results[randomIndex].url;
-      const pokemonResponse = await axios.get(randomPokemonUrl);
-      setRandomPokemon(pokemonResponse.data);
+      const pokemons = await Promise.all(
+        response.data.results.map(async (result) => {
+          const pokemonResponse = await axios.get(result.url);
+          return pokemonResponse.data;
+        })
+      );
+      setRandomPokemons(pokemons);
     } catch (error) {
-      console.error("Error fetching random pokemon:", error);
+      console.error("Error fetching random pokemons:", error);
     }
   };
 
-  const handleAddToCollection = () => {
-    if (randomPokemon) {
-      navigation.navigate("Collection", { pokemon: randomPokemon });
-    }
+  const handleAddToCollection = (pokemon) => {
+    navigation.navigate("Collection", { pokemon });
   };
+
+const renderItem = ({ item }) => {
+  return (
+    <View style={{ alignItems: "center", marginBottom: moderateScale(20) }}>
+      <Image
+        style={{ width: moderateScale(200), height: moderateScale(200) }}
+        source={{ uri: item.sprites.front_default }}
+      />
+      <Text>{item.name}</Text>
+      <Button
+        title="Add to Collection"
+        onPress={() => handleAddToCollection(item)}
+      />
+    </View>
+  );
+};
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      {randomPokemon && (
-        <>
-          <Image
-            style={{ width: 200, height: 200 }}
-            source={{ uri: randomPokemon.sprites.front_default }}
-          />
-          <Text>{randomPokemon.name}</Text>
-          <Button title="Add to Collection" onPress={handleAddToCollection} />
-        </>
-      )}
+      <FlatList
+        data={randomPokemons}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 };
